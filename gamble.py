6d6b4eb958f9200ebf56fd942e2c503abcefb047
blueshadow0324 <blueshadow0324@gmail.com>
2026-04-20 11:26:42 +0200

    Commit 12

--- gamble.py
+++ gamble.py
@@ -18,17 +18,43 @@ def gamble(user=None):
     st.title("Gamble")
     st.text(f"Coins: {coins}")
     st.button("Gamble 5!", on_click=randomGamble, args=(user, coins))
+    st.divider()
+    st.button("Custom gambling!", on_click=custom, args=(user, coins))
 
 def randomGamble(user=None, coins=None):
-    int = random.randint(1, 5)
-    if int == 5:
-        coins += 25
-        db.table("users") \
-            .update({"coins": coins}) \
-            .eq("username", user) \
-            .execute()
-        st.warning("You won 25!")
+    if coins > 5:
+        int = random.randint(1, 5)
+        if int == 5:
+            coins += 25
+            db.table("users") \
+                .update({"coins": coins}) \
+                .eq("username", user) \
+                .execute()
+            st.warning("You won 25!")
+        else:
+            db.table("users") \
+                .update({"coins": coins - 5}).eq("username", user).execute()
+            st.warning("You lost 5!")
     else:
-        db.table("users") \
-           .update({"coins": coins-5}) .eq("username", user) .execute()
-        st.warning("You lost 5!")
+        st.warning("You dont have enough coins!")
+def custom(user=None, coins=None):
+    res = db.table("users").select("*").execute()
+    userCoins = {row["username"]: row["coins"] for row in res.data}
+    coins = userCoins[user]
+    st.title("Custom gambling")
+    st.text(f"Coins: {coins}")
+    amount = st.number_input("Enter amount to gamble:")
+    odds = st.number_input("Gamble odds (ex 5, 10, 100):")
+    st.button("Custom gamble!", on_click=customGamble, args=(user, coins, amount, odds))
+
+def customGamble(user, coins, amount, odds):
+    if coins > amount:
+        int = random.randint(1, odds)
+        if int == 1:
+            coins += amount * odds
+            st.warning(f"you won {amount*odds}!")
+        else:
+            coins -= amount
+            st.warning(f"You lost {amount}")
+    else:
+        st.warning("You dont have enough coins!")
